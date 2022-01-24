79f0c718dee4f2034f9c23a969c9ae7827c8347e
Martin Mena <martinmena@outlook.com>
2022-01-24 16:13:51 -0500

    Refactor errors_controller.rb

--- app/controllers/errors_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Controller that renders custom errors
-class ErrorsController < ApplicationController
-  def not_found
-    render json: { message: 'Not Found' }, status: :not_found
-  end
-end
